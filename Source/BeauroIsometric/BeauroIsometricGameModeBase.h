9c9dd1d5814bf21e245a1f3d12248d0808b3256d
HYDROK-MK4\hydrok_MK4 <dave.hardie@gmail.com>
2020-05-09 11:59:17 -0400

    First content including isometric sheets and collision. Movement and camera angle working, as well as sight sense for AI.

---
+++ Source/BeauroIsometric/BeauroIsometricGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BeauroIsometricGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEAUROISOMETRIC_API ABeauroIsometricGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
